df806535f294946201383202af970952524987e7
Jay Ostinowsky <dukebdfan@comcast.net>
2013-05-24 18:36:01 -0400

    Added Basic Play/Stop

---
+++ freezing-tyrion/Main.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace freezing_tyrion
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Playbtn = new System.Windows.Forms.Button();
+            this.Stopbtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Playbtn
+            // 
+            this.Playbtn.Location = new System.Drawing.Point(80, 93);
+            this.Playbtn.Name = "Playbtn";
+            this.Playbtn.Size = new System.Drawing.Size(75, 23);
+            this.Playbtn.TabIndex = 0;
+            this.Playbtn.Text = "Play";
+            this.Playbtn.UseVisualStyleBackColor = true;
+            this.Playbtn.Click += new System.EventHandler(this.Playbtn_Click);
+            // 
+            // Stopbtn
+            // 
+            this.Stopbtn.Location = new System.Drawing.Point(179, 93);
+            this.Stopbtn.Name = "Stopbtn";
+            this.Stopbtn.Size = new System.Drawing.Size(75, 23);
+            this.Stopbtn.TabIndex = 1;
+            this.Stopbtn.Text = "Stop";
+            this.Stopbtn.UseVisualStyleBackColor = true;
+            this.Stopbtn.Click += new System.EventHandler(this.Stopbtn_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(446, 293);
+            this.Controls.Add(this.Stopbtn);
+            this.Controls.Add(this.Playbtn);
+            this.Name = "Main";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Playbtn;
+        private System.Windows.Forms.Button Stopbtn;
+    }
+}
+
---
+++ freezing-tyrion/Main.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace freezing_tyrion
+{
+    public partial class Main : Form
+    {
+        WMPLib.WindowsMediaPlayer wplayer;
+        public Main()
+        {
+            wplayer = new WMPLib.WindowsMediaPlayer();
+            InitializeComponent();
+        }
+
+        private void Playbtn_Click(object sender, EventArgs e)
+        {
+            string path = @"G:\Music\Daft Punk\Random Access Memories\";
+            string mp3 = "";
+            DirectoryInfo dir = new DirectoryInfo(path);
+            foreach (var file in dir.GetFiles())
+            {
+                if (file.Name.Contains("Lucky"))
+                {
+                    mp3 = file.Directory.ToString() + "\\" + file.ToString();
+                }
+            }
+            
+            wplayer.URL = mp3;
+            wplayer.controls.play();
+        }
+
+        private void Stopbtn_Click(object sender, EventArgs e)
+        {
+            wplayer.controls.stop();
+        }
+    }
+}
