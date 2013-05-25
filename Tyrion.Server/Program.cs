1ae7af22b2da7bcfd281a6b675bc8800d097d878
Jay Ostinowsky <dukebdfan@comcast.net>
2013-05-25 19:15:07 -0400

    Started Server

---
+++ Tyrion.Server/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyrion.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress address = IPAddress.Parse("127.0.0.1");
+            IPEndPoint localEndpoint = new IPEndPoint(address, 5000);
+            listener.Bind(localEndpoint);
+            listener.Listen(100);
+            Socket s = listener.Accept();
+            
+        }
+    }
+}
